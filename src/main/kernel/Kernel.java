483eb5012a556880996c27f0cd4c30045454a2f4
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-05-19 20:01:12 +0300

    Reordered process creation to be inline with real OS & further developed output messages

--- src/main/kernel/Kernel.java
+++ src/main/kernel/Kernel.java
@@ -1,14 +1,11 @@
 package main.kernel;
 
-import main.elements.Interpreter;
-import main.elements.ProcessMemoryImage;
-import main.elements.Memory;
-import main.elements.Mutex;
+import main.elements.*;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.*;
 
 public class Kernel {
     private static Mutex userInputMutex;
@@ -30,30 +27,30 @@ public class Kernel {
     public static Memory getMemory() {return memory;}
     public static SystemCallHandler getSystemCallHandler() {return systemCallHandler;}
 
-    public Kernel(int roundRobinTimeSlice){
+    public Kernel(int instructionsPerTimeSlice){
         userInputMutex = new Mutex();
         userOutputMutex = new Mutex();
         fileMutex = new Mutex();
         interpreter = new Interpreter();
-        scheduler = new Scheduler(roundRobinTimeSlice);
+        scheduler = new Scheduler(instructionsPerTimeSlice);
         memory = new Memory();
         systemCallHandler = new SystemCallHandler();
     }
 
     public static void initDefault(){
-        int INSTRUCTIONS_PER_TIME_SLICE = 2;
+        final int INSTRUCTIONS_PER_TIME_SLICE = 2;
         System.out.print("Instructions per time slice:" + INSTRUCTIONS_PER_TIME_SLICE + "\n");
 
         float[] arrivalTimes = {0,1,4};
         String[] arrivalLocations = {"src/program_files/Program_1.txt",
                                      "src/program_files/Program_2.txt",
                                      "src/program_files/Program_3.txt"
-        };
+                                    };
         kernel = new Kernel(2);
 
 //      Beginning counter representing time, incrementing timer until the first process scheduled to arrive arrives
         Counter counter = new Counter();
-        System.out.println("Initializing System Timer.. \n");
+        System.out.println("\nInitializing System Timer.. \n");
         counter.run(arrivalTimes, arrivalLocations);
     }
 
@@ -62,18 +59,37 @@ public class Kernel {
         int instructionsPerTimeSlice;
 
         System.out.print("Enter the instructions per time slice (round robin): \n");
-        instructionsPerTimeSlice = inp.nextInt();
+        try {
+            instructionsPerTimeSlice = inp.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Instructions per time slice must be an integer.\nExiting ...");
+            return;
+        }
+        if(instructionsPerTimeSlice<=0){
+            System.out.println("ERROR: Instructions per time slice must be greater than zero.\nExiting ...");
+            return;
+        }
 
         float[] arrivalTimes = new float[3];
         String[] arrivalLocations = {"src/program_files/Program_1.txt",
                                      "src/program_files/Program_2.txt",
                                      "src/program_files/Program_3.txt"
-        };
+                                    };
         kernel = new Kernel(instructionsPerTimeSlice);
 
-        System.out.println("Enter the arrival time of P1, P2 and P3 in order respectively (integer only)");
-        for(int i = 0; i < 3; i++)
-            arrivalTimes[i] = (inp.nextInt());
+        System.out.println("Enter the arrival times of P1, P2 and P3 in order respectively (Integer only)");
+        for(int i = 0; i < 3; i++) {
+            try {
+                arrivalTimes[i] = (inp.nextInt());
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Arrival time must be an integer.\nExiting ...");
+                return;
+            }
+            if(arrivalTimes[i]<0){
+                System.out.println("ERROR: Arrival time cannot be negative.\nExiting ...");
+                return;
+            }
+        }
 
 //      Sort arrival times and arrived processes in order
         for (int i = 0; i < arrivalTimes.length - 1; i++) {
@@ -92,7 +108,7 @@ public class Kernel {
 
 //      Beginning counter representing time, incrementing timer until the first process scheduled to arrive arrives
         Counter counter = new Counter();
-        System.out.println("Initializing System Timer.. \n");
+        System.out.println("\nInitializing System Timer.. \n");
         counter.run(arrivalTimes, arrivalLocations);
 
     }
@@ -131,27 +147,42 @@ public class Kernel {
         return result;
     }
 
+    /**
+     * Implementation of Process creation here follows
+     * (as much as possible) the real procedure used in
+     * real OS process creation.
+     */
     public synchronized void createNewProcess(String programFilePath) {
-//      Create ProcessMemoryImage and arrive at Scheduler
         Object[] canFitInMemory = canFitInMemory(programFilePath);
-        ProcessMemoryImage p = new ProcessMemoryImage( (Integer) canFitInMemory[1],
-                                                       (Integer) canFitInMemory[2],
-                                                       Kernel.getInterpreter().getInstructionsFromFile(programFilePath) );
-        Kernel.getScheduler().addArrivedProcess(p);
-        Kernel.getScheduler().addToReadyQueue(p);
-        Kernel.getScheduler().addBurstTime( (Integer) canFitInMemory[3] );
+
+//      Begin by getting next unique PID from the scheduler
+        System.out.println("    Acquiring unique PID");
+        int processID = Kernel.getScheduler().getNextProcessID();
 
         if( (boolean) canFitInMemory[0] ){
-//          Allocate block of memory for Process
-            p.getPCB().setLowerMemoryBoundary( (Integer) canFitInMemory[1] );
-            p.getPCB().setUpperMemoryBoundary( (Integer) canFitInMemory[2] );
-            Kernel.getMemory().allocateMemoryPartition(p, (Integer) canFitInMemory[1], (Integer) canFitInMemory[2]);
-            Kernel.getScheduler().getInMemoryProcesses().add(p);
-            System.out.println("Process added to memory");
+//          Allocate memory space for the process
+            System.out.println("    Allocating memory space");
+            Kernel.getMemory().allocateMemoryPartition((Integer) canFitInMemory[1], (Integer) canFitInMemory[2]);
 
-//          Finalize Process creation ???
-            //
-            System.out.println("Process created successfully \n");
+//          Initialize the Process Control Block
+            System.out.println("    Initializing PCB");
+            ProcessControlBlock pcb = new ProcessControlBlock(processID, (Integer) canFitInMemory[1], (Integer) canFitInMemory[2]);
+
+//          Linking process to scheduling queue and changing process state from 'NEW' to 'READY'
+//          and creating other data structures (ProcessMemoryImage object)
+            System.out.println("    Linking to scheduling queue");
+            ProcessMemoryImage p = new ProcessMemoryImage( (Integer) canFitInMemory[1], (Integer) canFitInMemory[2], Kernel.getInterpreter().getInstructionsFromFile(programFilePath) );
+            p.setProcessControlBlock(pcb);
+            Kernel.getScheduler().addArrivedProcess(p);
+            Kernel.getScheduler().addToReadyQueue(p);
+            Kernel.getScheduler().addBurstTime( (Integer) canFitInMemory[3] );
+
+//          Finalize Process creation
+            System.out.println("    Finalizing process creation");
+            Kernel.getMemory().fillMemoryPartition(p, (Integer) canFitInMemory[1], (Integer) canFitInMemory[2]);
+            Kernel.getScheduler().getInMemoryProcesses().add(p);
+
+            System.out.println("    Process created successfully\n");
         }
 
         else {
@@ -164,11 +195,11 @@ public class Kernel {
              * move our new process to memory
              */
 
-            System.out.println("Process added to memory");
+            //System.out.println("Process added to memory.");
 
 //          Finalize Process creation ???
             //
-            System.out.println("Process created successfully \n");
+            //System.out.println("Process created successfully.\n");
         }
     }
 
@@ -176,7 +207,6 @@ public class Kernel {
         private float count = 0;
 
         public void run(float[] arrivalTimes, String[] arrivalLocations) {
-            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(3);
             final int[] i = {0};
             List<Thread> threads = new ArrayList<>();
 
@@ -184,13 +214,11 @@ public class Kernel {
                 synchronized (this) {
                     float round = (float) ( Math.round(count * 10.0) / 10.0);
                     while (i[0] < arrivalTimes.length && round == arrivalTimes[i[0]]) {
-                        System.out.println("Process " + arrivalLocations[i[0]]+ " arrived @ Time = " + count);
+                        System.out.println("Process \"" + arrivalLocations[i[0]]+ "\" arrived at time = " + arrivalTimes[i[0]] + " sec");
                         final int locationIndex = i[0];
                         Thread thread = new Thread(() -> kernel.createNewProcess(arrivalLocations[locationIndex]));
                         thread.start();
                         threads.add(thread);
-
-                        //scheduler.submit(() -> kernel.createNewProcess(arrivalLocations[locationIndex]));
                         i[0]++;
                     }
                     if(i[0] == arrivalTimes.length) {
@@ -202,10 +230,13 @@ public class Kernel {
                                 e.printStackTrace();
                             }
                         }
-                        System.out.println("All processes successfully created! \n");
+                        System.out.println("\n*************************************\n");
+                        System.out.println("All processes successfully created!");
+                        System.out.println("\n*************************************\n");
                         System.out.println(Kernel.getMemory());
                         Kernel.getScheduler().printReadyQueue();
                         Kernel.getScheduler().printBlockedQueue();
+                        System.out.println("\n*************************************");
                         return;
                     }
                     count += 0.2;
