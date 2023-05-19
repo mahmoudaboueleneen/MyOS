a37cae8acaa6696093274fe2f1169491ba353c37
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-05-19 10:20:38 +0300

    Added another custom constructor

--- src/main/exceptions/InvalidInstructionException.java
+++ src/main/exceptions/InvalidInstructionException.java
@@ -1,7 +1,6 @@
 package main.exceptions;
 
 public class InvalidInstructionException extends Exception{
-    public InvalidInstructionException() {
-        super("Invalid instruction syntax.");
-    }
+    public InvalidInstructionException() {super("Invalid instruction syntax.");}
+    public InvalidInstructionException(String message) {super(message);}
 }
